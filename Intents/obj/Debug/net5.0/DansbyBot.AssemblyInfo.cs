d7d433f2aecbf481a177121c6f6ac7a91569fb91
IceTeaOnMac <kjgbusiness8@gmail.com>
2024-05-06 23:31:25 -0400

    Config for cross platform .Net OS support

--- Intents/obj/Debug/net5.0/DansbyBot.AssemblyInfo.cs
+++ Intents/obj/Debug/net5.0/DansbyBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DansbyBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea290d03aaa1dfd1e5e2923426641df77eb22f0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+490be90851fa497b5c86915fdbac780500087221")]
 [assembly: System.Reflection.AssemblyProductAttribute("DansbyBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DansbyBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
