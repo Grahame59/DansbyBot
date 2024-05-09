d384d9beb0282b60bd8db15ac550c9f86c8f6099
Grahame59 <152683573+Grahame59@users.noreply.github.com>
2024-05-09 18:06:57 -0400

    Updates on Intents

--- Intents/obj/Debug/net6.0/DansbyBot.AssemblyInfo.cs
+++ Intents/obj/Debug/net6.0/DansbyBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DansbyBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5aacc3d3abee22c7f3b62a454c59a33d5cb031e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca05c62040fc784030b42a08f1e4a02e32990514")]
 [assembly: System.Reflection.AssemblyProductAttribute("DansbyBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DansbyBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
