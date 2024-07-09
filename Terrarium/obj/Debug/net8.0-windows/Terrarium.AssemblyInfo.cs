1951ed1f5bda2cb40ca1990aa0fef4eb41361a8c
Grahame59 <152683573+Grahame59@users.noreply.github.com>
2024-07-08 22:13:43 -0400

    Restucture for New .sln

--- Terrarium/obj/Debug/net8.0-windows/Terrarium.AssemblyInfo.cs
+++ Terrarium/obj/Debug/net8.0-windows/Terrarium.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("DansbyBot")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Terrarium")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8cbde8ea21345e949259062095bf7334ae9c0c2")]
-[assembly: System.Reflection.AssemblyProductAttribute("DansbyBot")]
-[assembly: System.Reflection.AssemblyTitleAttribute("DansbyBot")]
+[assembly: System.Reflection.AssemblyProductAttribute("Terrarium")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Terrarium")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
