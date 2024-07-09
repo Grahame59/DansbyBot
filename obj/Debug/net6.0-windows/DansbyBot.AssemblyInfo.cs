f3986266a915527cdc30d4b5a74b1c560c4815e4
Grahame59 <152683573+Grahame59@users.noreply.github.com>
2024-07-08 23:28:47 -0400

    REVERT COMPLETE

--- obj/Debug/net6.0-windows/DansbyBot.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/DansbyBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DansbyBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fab3389f2eb7e3461e96378602cf48f62544d7b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f44bffd1edd09ab94afeb3ac13033f10802192cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("DansbyBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DansbyBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
