6b16ec19398d481bd300ca63715cdd2a19ac5b8f
Grahame59 <152683573+Grahame59@users.noreply.github.com>
2024-07-31 21:57:06 -0400

    UPD

--- obj/Debug/net6.0-windows/DansbyBot.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/DansbyBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DansbyBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13930bfa3438364cb1fb29c6455d63a805d7574b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a8468c1239f6f89812da330669443bef6bedee0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DansbyBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DansbyBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
