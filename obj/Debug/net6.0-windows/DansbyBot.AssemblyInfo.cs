2f60f12fca4ba4aa5cef79a8aee0ed0c06603f1f
Grahame59 <152683573+Grahame59@users.noreply.github.com>
2025-01-08 23:06:11 -0500

    some minor Intent Mapping Updates

--- obj/Debug/net6.0-windows/DansbyBot.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/DansbyBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DansbyBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+349203b257f0589622a87edea31118e51b8f5418")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c23829db581d537e425a26d4b584e469b7db5cf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DansbyBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DansbyBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
